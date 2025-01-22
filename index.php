9b7adac3439a921476601884805de880ae904c5f
Eckstein <zac@zaceckstein.com>
2025-01-22 12:59:04 -0800

    Add homepage sidebar and widgets

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php get_header(); ?>
 
-<main id="primary" class="site-main container">
-    <?php if (have_posts()): ?>
-        <?php
-        // Display sticky posts first
-        $sticky = get_option('sticky_posts');
-        if (!empty($sticky)) {
-            $sticky_args = array(
-                'posts_per_page' => -1,
-                'post__in' => $sticky,
-                'ignore_sticky_posts' => 1
-            );
-            $sticky_query = new WP_Query($sticky_args);
-
-            if ($sticky_query->have_posts()):
-                while ($sticky_query->have_posts()): $sticky_query->the_post();
-                    ?>
-                    <article id="post-<?php the_ID(); ?>" <?php post_class('sticky-post'); ?>>
+<div class="site-content-wrapper container">
+    <main id="primary" class="site-main">
+        <?php if (have_posts()): ?>
+            <?php
+            // Display regular posts
+            while (have_posts()): the_post();
+                if (!is_sticky()): ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                         <header class="entry-header">
                             <?php if (has_post_thumbnail()): ?>
                                 <div class="post-thumbnail">
@@ -50,64 +41,26 @@
                             </a>
                         </div>
                     </article>
-                <?php
-                endwhile;
-                wp_reset_postdata();
-            endif;
-        }
-        ?>
+                <?php endif;
+            endwhile;
 
-        <?php
-        // Display regular posts
-        while (have_posts()): the_post();
-            if (!is_sticky()): ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <?php if (has_post_thumbnail()): ?>
-                            <div class="post-thumbnail">
-                                <?php the_post_thumbnail('large'); ?>
-                            </div>
-                        <?php endif; ?>
+            the_posts_pagination(array(
+                'mid_size' => 2,
+                'prev_text' => __('Previous', 'lclookout'),
+                'next_text' => __('Next', 'lclookout'),
+            ));
 
-                        <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        </h2>
+        else:
+            ?>
+            <p><?php esc_html_e('No posts found.', 'lclookout'); ?></p>
+        <?php endif; ?>
+    </main>
 
-                        <div class="entry-meta">
-                            <span class="posted-on">
-                                <?php echo get_the_date(); ?>
-                            </span>
-                            <span class="byline">
-                                <?php the_author_posts_link(); ?>
-                            </span>
-                            <?php if (has_category()): ?>
-                                <span class="categories">
-                                    <?php the_category(', '); ?>
-                                </span>
-                            <?php endif; ?>
-                        </div>
-                    </header>
-
-                    <div class="entry-content">
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>" class="read-more">
-                            <?php esc_html_e('Read More', 'lclookout'); ?>
-                        </a>
-                    </div>
-                </article>
-            <?php endif;
-        endwhile;
-
-        the_posts_pagination(array(
-            'mid_size' => 2,
-            'prev_text' => __('Previous', 'lclookout'),
-            'next_text' => __('Next', 'lclookout'),
-        ));
-
-    else:
-        ?>
-        <p><?php esc_html_e('No posts found.', 'lclookout'); ?></p>
+    <?php if (is_home()): ?>
+        <aside id="secondary" class="widget-area">
+            <?php dynamic_sidebar('homepage-sidebar'); ?>
+        </aside>
     <?php endif; ?>
-</main>
+</div>
 
 <?php get_footer(); ?> 
