26a22c03b7a95d6419558703a23894a87e5f27e1
Eckstein <zac@zaceckstein.com>
2025-01-22 16:51:20 -0800

    Refactor search template to display excerpts instead of full content, enhancing loading speed and user experience. Removed post thumbnail display for a cleaner layout.

--- search.php
+++ search.php
@@ -35,11 +35,7 @@
                 ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
                     <header class="entry-header">
-                        <?php if (has_post_thumbnail()): ?>
-                            <div class="post-thumbnail">
-                                <?php the_post_thumbnail('large'); ?>
-                            </div>
-                        <?php endif; ?>
+                        
 
                         <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
@@ -59,7 +55,7 @@
                     </header>
 
                     <div class="entry-content">
-                        <?php the_content(esc_html__('Keep reading...', 'lclookout')); ?>
+                        <?php the_excerpt(); ?>
                     </div>
                 </article>
             <?php endwhile; ?>
